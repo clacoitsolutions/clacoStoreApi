029aa40a7b75623d50c4e7a4b3f920fa1208f77e
clacoitsolutions <clacoitsolutions@gmail.com>
2024-05-09 12:58:33 +0530

    resolve server.js conflict

--- server.js
+++ server.js
@@ -13,16 +13,10 @@ import getorderlist from "./routes/OrderListRoute.js";
 import { dbConfig } from "./config/db.js"; // Import the database configuration
 import trackingorderRoute from "./routes/trackingorderRoute.js";
 import mywalletRoute from "./routes/mywalletRoute.js";
-<<<<<<< HEAD
 import SearchRoute from "./routes/SearchRoute.js";
 import FillterRoute from "./routes/FillterRoute.js";
 
 const {ConnectionPool} = pkg
-=======
-
-const { ConnectionPool } = pkg;
->>>>>>> f49bce6754061d18bc0a7ced7d95d7955f9c09d3
-
 const app = express();
 
 dotenv.config();
@@ -55,21 +49,12 @@ app.use((req, res, next) => {
 // Import and use the product route
 app.use("/product", exampleRoute);
 app.use("/category", categoryRoute);
-<<<<<<< HEAD
 app.use("/",userRoute);
 app.use("/",bannerApiRoute);
 app.use("/",productCategoryRoute);
 app.use("/",SearchRoute);
 app.use("/",FillterRoute);
 app.use("/",trackingorderRoute);
-
-app.get("/", (req,res)=>{
-    res.send("hello claco team")
-})
-=======
-app.use("/", userRoute);
-app.use("/", bannerApiRoute);
-app.use("/", productCategoryRoute);
 app.use("/", deliverAdress);
 app.use("/", GetCity);
 app.use("/", getorderlist);
@@ -79,7 +64,6 @@ app.use("/", mywalletRoute);
 app.get("/", (req, res) => {
     res.send("hello claco team");
 });
->>>>>>> f49bce6754061d18bc0a7ced7d95d7955f9c09d3
 
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
