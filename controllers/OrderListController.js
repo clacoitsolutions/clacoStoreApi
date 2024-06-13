8fc405deeb8be26c090ec68c79f4dbc1d040eab0
Niranjanpaswan11 <Niranjanpaswan1511@gmail.com>
2024-06-13 12:40:02 +0530

    OrderDetails On Click create Api Design

--- controllers/OrderListController.js
+++ controllers/OrderListController.js
@@ -56,3 +56,36 @@ export const deletecart = async (req,res)=>{
         req.status(500).json({error:"Internal Srver Error"});
     }
 }
+
+export const clickorderdetails = async (req, res) => {
+    try {
+        // Validate input parameters
+        const { orderId } = req.body;
+
+        const pool = req.pool;
+        await pool.connect();
+        const request = pool.request();
+
+        // Set up input parameters for the stored procedure
+        request.input("Action", "95");
+        request.input("orderId", orderId);
+        // request.input("OrderId", orderId);
+
+        // Execute the stored procedure
+        const result = await request.execute("proc_BindCustomerDashBoard");
+
+        // Retrieve the order items from the result
+        const orderItems = result.recordset; // Assuming the returned data is in the form of a recordset
+
+        // Send a success response along with the returned data and a success message
+        res.status(200).json({ message: 'Order items retrieved successfully', orderItems: orderItems });
+
+    } catch (error) {
+        // Handle errors
+        console.error("SQL error", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+
--- routes/OrderListRoute.js
+++ routes/OrderListRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { orderlistcontroller,deletecart } from "../controllers/OrderListController.js";
+import { orderlistcontroller,deletecart ,clickorderdetails} from "../controllers/OrderListController.js";
 
 const router = express.Router();
 
 // Route to get order items
 router.post("/getorderitems", orderlistcontroller); // Assuming you want to use POST method
 
-router.delete("/deletecart",deletecart)
+router.delete("/deletecart",deletecart);
+
+router.post("/clickorderdetails",clickorderdetails)
 
 export default router;
